9c14d55837c90eb28971a586b438bca1d3ad6068
PAUL <pziarczyk@gmail.com>
2019-10-18 18:42:10 +0200

    footer part

---
+++ footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import './App.css'
+
+class Foot extends Component {
+    render() {
+        return (
+            <div className="footer">
+                <div className="footer-copyright">
+                Information additionnelle
+                </div>
+                <p className="footer-copyright">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque interdum quam odio, quis placerat ante luctus eu.
+                Sed aliquet dolor id sapien rutrum, id vulputate quam iaculis.</p>
+            </div>
+        )
+    }
+}
+
+export default Foot
