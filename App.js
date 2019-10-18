f44d7c674817883062e6fc4d59978a8c661d58a3
PAUL <PAUL@MacBook-Air-de-Paul.local>
2019-10-18 16:30:12 +0200

    init

---
+++ App.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react'
+import './App.css'
+import Log from './log.js'
+import Head from './header.js'
+import Foot from './footer.js'
+
+class App extends Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      articles: []
+    }
+  }
+
+  addArticles = (article) => {
+    console.log('articles: ' + article.designation)
+  
+  }
+
+  render() {
+    return (
+      <div className="body">
+        <Head>
+        </Head>
+        <Log formTitle="LOG IN" addArticles={this.addArticles}/>
+        <Foot>
+        </Foot>
+      </div>
+    )
+  }
+
+}
+
+export default App
